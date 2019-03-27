0ea4091453b8d8434be4c56f1ecc19685b40a2ff
maruware <me@maruware.com>
2019-03-27 16:06:50 +0900

    pass auth-koa test

---
+++ packages/auth-koa/src/index.ts
@@ -0,0 +1,69 @@
+import Koa, {Middleware} from 'koa'
+import Router from 'koa-router'
+import jwt from 'jsonwebtoken'
+
+declare module "koa" {
+  interface Request extends Koa.BaseRequest {
+      body?: any;
+  }
+}
+
+interface AdminUser {
+  username: string
+  password: string
+}
+
+interface BuildOptions {
+  expiresIn: string
+}
+export const buildAuth = (jwtSecret: string, admins: AdminUser[], options: BuildOptions = {expiresIn: '2d'}) => {
+  const router = new Router()
+
+  router.post('/', async ctx => {
+    const { username, password } = ctx.request.body
+
+    const admin = admins.find(a => a.username === username && a.password === password)
+    if (!admin) {
+      ctx.throw(401, { error: 'Unauthorized' })
+    }
+
+    ctx.body = {
+      token: jwt.sign({ username: admin.username }, jwtSecret, {
+        expiresIn: options.expiresIn
+      })
+    }
+  })
+  
+  router.get('/verify', async (ctx) => {
+    const token = ctx.req.headers.authorization
+    if (verifyJwt(token)) {
+      ctx.body = 'ok'
+    } else {
+      ctx.throw(401, 'Unauthorized')
+    }
+  })
+  
+  const verifyJwt = (token: string) => {
+    try {
+      const decoded: any = jwt.verify(token, jwtSecret)
+      const admin = admins.find(a => a.username === decoded.username)
+
+      return Boolean(admin)
+    } catch (e) {
+      return false
+    }
+  }
+
+  const middleware: Middleware = async (ctx, next) => {
+    const token = ctx.req.headers.authorization
+    if (verifyJwt(token)) {
+      await next()
+    } else {
+      ctx.throw(401, 'Unauthorized')
+    }
+  }
+  
+  return {router, middleware}
+}
+
+
---
+++ packages/auth-koa/test/index.test.ts
@@ -0,0 +1,52 @@
+import Koa from 'koa'
+import koaBody from 'koa-body'
+import Router from 'koa-router'
+
+import request from 'supertest'
+import {buildAuth} from '../src/index'
+
+const setupServer = () => {
+  const app = new Koa()
+  app.use(koaBody())
+  return app
+}
+
+describe('User Test', () => {
+  it('basic', async () => {
+    const app = setupServer()
+    const admins = [{username: 'takashi', password: 'my_password'}]
+    const auth = buildAuth('my secret', admins)
+
+    const router = new Router()
+    router.use('/auth', auth.router.routes(), auth.router.allowedMethods())
+
+    const apiRouter = new Router()
+    apiRouter.get('/sample', async ctx => {
+        ctx.body = 'ok'
+    })
+    router.use(auth.middleware, apiRouter.routes(), apiRouter.allowedMethods())
+
+    app.use(router.routes()).use(router.allowedMethods())
+
+    const server = app.callback()
+
+    let res = await request(server).get('/sample')
+    expect(res.status).toBe(401)
+
+    res = await request(server).get('/auth/verify')
+    expect(res.status).toBe(401)
+
+    res = await request(server).post('/auth').send(admins[0])
+    expect(res.status).toBe(200)
+    expect(res.body.token).toBeTruthy()
+
+    let {token} = res.body
+
+    res = await request(server).get('/sample').set('Authorization', token)
+    expect(res.status).toBe(200)
+
+    res = await request(server).get('/auth/verify').set('Authorization', token)
+    expect(res.status).toBe(200)
+    
+  })
+})
